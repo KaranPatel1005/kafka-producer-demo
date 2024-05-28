67930429c0ee08e00c046bef115d749a2d2ac859
KaranPatel1005 <kgkaran97@gmail.com>
2024-05-28 18:03:16 +0530

    added response in test api

--- routes/index.ts
+++ routes/index.ts
@@ -1,11 +1,14 @@
+import { TOPICS } from "../kafka/kafka";
 import { sendMessage } from "../kafka/setup";
 import express, { NextFunction, Request, Response } from "express";
 const router = express.Router();
 
 /* GET home page. */
 router.get("/send", function (req: Request, res: Response, next: NextFunction) {
-  sendMessage;
-  res.render("index", { title: "Express" });
+  sendMessage(TOPICS.topic1, JSON.stringify({ message: "hi" }));
+  res.status(200).json({
+    status: true,
+  });
 });
 
 export default router;
